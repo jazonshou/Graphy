13e103b922808badc6eb855a6da2049b51ce0b8b
Jason <24jasonz@students.tas.tw>
2022-08-02 17:49:43 +0800

    [fix] ???

---
+++ include/EZGraphLib/Grapher.hpp
@@ -0,0 +1,62 @@
+#pragma once
+#include "TaskWrapper.hpp"
+#include "okapi/api/units/QTime.hpp"
+#include "pros/screen.hpp"
+#include <map>
+#include <string>
+
+#define MAX_CACHE_SIZE 430
+#define GRAPH_TOP 40
+#define GRAPH_BOTTOM 215
+#define GRAPH_LEFT 25
+#define GRAPH_RIGHT 455
+
+class AsyncGrapher : public TaskWrapper {
+    private: 
+    std::map<std::string, std::vector<double>> container;
+    std::map<std::string, uint32_t> colors;
+    std::string title;
+    okapi::QTime refreshRate;
+
+    public: 
+    /**
+     * @brief Construct a new Async Grapher object
+     * 
+     * @param title graph title
+     * @param rate refresh rate
+     */
+    AsyncGrapher(const std::string &title, const okapi::QTime &rate = 10 * okapi::millisecond);
+
+    /**
+     * @brief Add new graph data type
+     * 
+     * @param name data type name
+     * @param color line color
+     */
+    void addDataType(const std::string &name, const uint32_t color); 
+
+    /**
+     * @brief Update graph
+     * 
+     * @param name data type name
+     * @param val updated data value
+     */
+    void update(const std::string &name, double val);
+
+    /**
+     * @brief Set the refresh rate
+     * 
+     * @param rate refresh rate
+     */
+    void setRefreshRate(const okapi::QTime &rate);
+
+    /**
+     * @brief Get the current refresh rate
+     * 
+     * @return refresh rate
+     */
+    okapi::QTime getRefreshRate();
+
+    protected:
+    void loop() override;
+};
