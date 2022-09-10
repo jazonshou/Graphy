82aa6683144c8a09006e3324c54e7659d85204c8
Jason <24jasonz@students.tas.tw>
2022-09-10 23:12:08 +0800

    [feat] remove irrelevant stuff & add conveniece

--- .d/EZGraphLib/TaskWrapper.cpp.d
+++
@@ -1,6 +0,0 @@
-bin/EZGraphLib/TaskWrapper.cpp.o: src/EZGraphLib/TaskWrapper.cpp \
- include/EZGraphLib/TaskWrapper.hpp include/pros/rtos.hpp \
- include/pros/rtos.h
-include/EZGraphLib/TaskWrapper.hpp:
-include/pros/rtos.hpp:
-include/pros/rtos.h:
